188261784026f8b49087f8d473c164148881699b
Elena Williams <github@elena.net.au>
2018-05-19 14:47:34 +1000

    Add first pass of inputs models and migrations

--- inputs/models.py
+++ inputs/models.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Sale(models.Model):
+
+        day = models.CharField(max_length=32)
+        date = models.DateField()
+        month = models.CharField(max_length=32)
+        value = models.FloatField()
+
+        week = models.IntegerField()
+
+
+class Hour(models.Model):
+
+        date = models.DateField()
+        value = models.FloatField()
