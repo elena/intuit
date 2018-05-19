6f5e5dae4fc08b29aefcacd5dd41fc05a6cb874d
Elena Williams <github@elena.net.au>
2018-05-19 16:53:02 +1000

    Tidy Sale model and add str and ordering

--- inputs/models.py
+++ inputs/models.py
@@ -5,15 +5,19 @@ from django.db import models
 
 class Sale(models.Model):
 
-        day = models.CharField(max_length=32)
-        date = models.DateField()
-        month = models.CharField(max_length=32)
-        value = models.FloatField()
+    day = models.CharField(max_length=32)
+    date = models.DateField()
+    month = models.CharField(max_length=32)
+    value = models.FloatField()
+    week = models.IntegerField()
 
-        week = models.IntegerField()
+    class Meta:
+        ordering = ['-week']
 
+    def __str__(self):
+            return self.date
 
 class Hour(models.Model):
 
-        date = models.DateField()
-        value = models.FloatField()
+    date = models.DateField()
+    value = models.FloatField()
