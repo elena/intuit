628fa0858ef53bc505b44ea64bf9af42ded90d54
Elena Williams <github@elena.net.au>
2018-05-19 18:43:12 +1000

    Refactor inputs admin

--- inputs/admin.py
+++ inputs/admin.py
@@ -3,8 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import Sale, Hour
+from .models import Sale, Resource, Week
+
 
 
 admin.site.register(Sale)
-admin.site.register(Hour)
+admin.site.register(Resource)
+admin.site.register(Week)
